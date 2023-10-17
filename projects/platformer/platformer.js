3d277ba7fc1352b3b7cccef403f1bad6859c8b2a
Eric briscoe <ebriscoe25@livingschoolnola.org>
2023-10-17 19:58:12 +0000

    turtles

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -28,12 +28,12 @@ $(function () {
      * Comment the lines out to remove the grid
      */
 
-    // for (let i = 100; i < canvas.width; i += 100) {
-    //   createPlatform(i, canvas.height, -1, -canvas.height);
-    // }
-    // for (let i = 100; i < canvas.height; i += 100) {
-    //   createPlatform(canvas.width, i, -canvas.width, -1);
-    // }
+     for (let i = 100; i < canvas.width; i += 100) {
+       createPlatform(i, canvas.height, -1, -canvas.height);
+     }
+     for (let i = 100; i < canvas.height; i += 100) {
+       createPlatform(canvas.width, i, -canvas.width, -1);
+     }
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
@@ -45,12 +45,37 @@ $(function () {
     // example usage: createPlatform(x,y,width,height)
 
 
-     createPlatform(0,647,100,10)
-     createPlatform(200,630, 100,10)
-     createPlatform(300.600,110,10)
-     createPlatform(099,637,100,10)
-     createPlatform()
-    // TODO 2
+     createPlatform(500,140,10,1000)
+     createPlatform(10,0,10,1000)
+     createPlatform(100,700,20,10)
+     createPlatform(200,640,20,10)
+     createPlatform(320,560,20,10)
+     createPlatform(489,450,20,10)
+     createPlatform(360,389,20,10)
+     createPlatform(200,340,20,10)
+     createPlatform(15,250,20,10)
+     createPlatform(100,130,410,10)
+     createCannon("bottom",225,600)
+     createCollectable("grace",265,20,1,1)
+
+       createPlatform(600,300,20,10)
+       createPlatform(770,450,20,10)
+       createPlatform(1400,140,10,1000)
+       createPlatform(1100,130,310,10)
+       createPlatform(950,600,20,10)
+       createPlatform(1070,500,20,10)
+       createPlatform(1190,400,20,10)
+       createPlatform(1290,320,20,10)
+       createPlatform(1000,260,190,10)
+       createPlatform(900,190,5,10)
+       createPlatform(1050,170,5,10)
+       createCannon("bottom",650,550)
+       createCannon("bottom",800,550)
+       createCannon("top",600,770)
+       createCannon("bottom",950,660)
+       createCannon("",400,850)
+       createCollectable("database",1300,5,1,1)
+     
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
